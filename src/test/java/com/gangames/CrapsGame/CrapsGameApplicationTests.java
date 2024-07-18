5aa081e1879fdbb533e339c5d076a5b2999eb8ba
amechkarov <mechkarov.alexander@gmail.com>
2024-07-18 20:21:28 +0300

    Initial project set up

---
+++ src/test/java/com/gangames/CrapsGame/CrapsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangames.CrapsGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrapsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
