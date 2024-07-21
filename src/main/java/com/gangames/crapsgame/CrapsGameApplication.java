f0e8455209dc8f0a67a56ec865fbb51a156ffb1b
amechkarov <mechkarov.alexander@gmail.com>
2024-07-21 21:17:41 +0300

    Renamed files

--- src/main/java/com/gangames/crapsgame/CrapsGameApplication.java
+++ src/main/java/com/gangames/crapsgame/CrapsGameApplication.java
@@ -1,4 +1,4 @@
-package com.gangames.CrapsGame;
+package com.gangames.crapsgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gangames/crapsgame/CrapsGameApplicationTests.java
+++ src/test/java/com/gangames/crapsgame/CrapsGameApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gangames.CrapsGame;
+package com.gangames.crapsgame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
